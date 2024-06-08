d4807d14245c9b95f7a119690b3ccae552a70817
zhangxz <zhangxz18@mails.tsinghua.edu.cn>
2024-06-08 14:34:48 +0800

    change for test

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,20 @@ import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
-<template>
+ <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="TST" />
     </div>
   </header>
 
   <main>
     <TheWelcome />
   </main>
-</template>
+ 
+</template> 
 
 <style scoped>
 header {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css'
 import App from './App.vue'
+import d3test from './d3test.vue'
+import menu from './menu.vue'
 
-createApp(App).mount('#app')
+
+const testapp = createApp(d3test)
+testapp.use(ElementPlus)
+testapp.mount('#app')
+
+const menuapp = createApp(menu)
+menuapp.use(ElementPlus)
+menuapp.mount('#menu')
+
+
+// createApp(App).mount('#app2')
